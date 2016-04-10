2baf37abe72cdb2b0ca3929d247dde2f3f2be0c3
evanlouden <evanlouden@gmail.com>
2016-04-10 12:57:30 -0400

    savedUser

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,14 +36,16 @@ class User < ActiveRecord::Base
     @login || self.username || self.email
   end
 
-  def self.find_for_database_authentication(warden_conditions)
-     conditions = warden_conditions.dup
-     if login = conditions.delete(:login)
-       where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-     elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-       where(conditions.to_hash).first
-     end
-     conditions[:email].downcase! if conditions[:email]
-     where(conditions.to_hash).first
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
   end
 end
