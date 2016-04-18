4d4353f38ffb5b86e04fa1f76d2ae988651aa6d1
Tommy Hui <hui.1tommy@gmail.com>
2016-04-18 13:56:42 -0400

    added evan's snippet

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,5 +1,5 @@
 class Preference < ActiveRecord::Base
-  
+
   belongs_to :user
   belongs_to :dwelling
 
@@ -31,5 +31,59 @@ class Preference < ActiveRecord::Base
 
   def full_name
     @full_name = "#{first_name} #{last_name}"
+  end
+  
+  def match_rating(preferences2)
+    match = 0
+    if self.smoking == preferences2.smoking
+      match += 2
+    elsif self.smoking == 0 || preferences2.smoking == 0
+      match += 1
+    else
+      match -= 2
+    end
+    if self.drinking == preferences2.drinking
+      match += 2
+    elsif self.drinking == 0 || preferences2.drinking == 0
+      match += 1
+    else
+      match -= 2
+    end
+    if self.music == preferences2.music
+      match += 2
+    elsif self.music == 0 || preferences2.music == 0
+      match += 1
+    else
+      match -= 2
+    end
+    if self.vegetarian == preferences2.vegetarian
+      match += 2
+    elsif self.vegetarian == 0 || preferences2.vegetarian == 0
+      match += 1
+    else
+      match -= 2
+    end
+    if self.cleanliness == preferences2.cleanliness
+      match += 2
+    elsif self.cleanliness == 0 || preferences2.cleanliness == 0
+      match += 1
+    else
+      match -= 2
+    end
+    if self.parties == preferences2.parties
+      match += 2
+    elsif self.parties == 0 || preferences2.parties == 0
+      match += 1
+    else
+      match -= 2
+    end
+    if self.sports == preferences2.sports
+      match += 2
+    elsif self.sports == 0 || preferences2.sports == 0
+      match += 1
+    else
+      match -= 2
+    end
+    match
   end
 end
